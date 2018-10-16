0cba073827bce3f0b4b9f1d314e6db4b4a3eef99
pengtao <tao.peng@aorise.org>
2018-10-17 00:34:16 +0800

    links

--- src/views/home/components/links.vue
+++ src/views/home/components/links.vue
@@ -1,7 +1,8 @@
 <template>
     <ul class="links lr clearfix" v-if="list.length > 0">
         <li class="talents-item" v-for="(item, index) in list" :key="'s-links' + index">
-            <a :href="item.link" :style="{backgroundImage: 'url(' + item.imgUrl + ')'}"></a>
+            <a :href="item.link">{{ item.name }}</a>
+            <!-- <a :href="item.link" :style="{backgroundImage: 'url(' + item.imgUrl + ')'}">{{ item.name }}</a> -->
         </li>
     </ul>
 </template>
@@ -25,9 +26,12 @@
             width: 49%;
             margin-right: 2%;
             margin-bottom: 5px;
-            height: 35px;
+            height: 30px;
+            line-height: 30px;
             float: left;
-            background-color: rgba(204, 204, 204, .3);
+            color: #333;
+            font-size: 13px;
+            // background-color: rgba(204, 204, 204, .3);
             a {
                 display: block;
                 width: 100%;
@@ -36,6 +40,11 @@
                 background-size: contain;
                 background-position: center center;
                 border-radius: 2px;
+                color: #333;
+                text-decoration: underline;
+                overflow: hidden;
+                text-overflow: ellipsis;
+                white-space: nowrap;
             }
             &:nth-child(2n) {
                 margin-right: 0;
